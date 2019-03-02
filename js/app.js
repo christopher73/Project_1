4c1a00cbe205878205e1cf7e92f3cc05bb5f393c
christopher fajardo <christopher.fajardo73@gmail.com>
2019-03-02 11:27:54 -0500

    adding html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-hello:
+(function() {
+  var config = {
+    apiKey: "AIzaSyBHFhFxaelIbZrC49uuNLPt4tDCoZx3gNM",
+    authDomain: "letstravel-956b6.firebaseapp.com",
+    databaseURL: "https://letstravel-956b6.firebaseio.com",
+    projectId: "letstravel-956b6",
+    storageBucket: "letstravel-956b6.appspot.com",
+    messagingSenderId: "1046260323930"
+  };
+  firebase.initializeApp(config);
+});
